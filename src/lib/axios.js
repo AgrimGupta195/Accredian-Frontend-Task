bc955ef3ae3a4ca26017cbf62b991665e5da1c1c
AgrimGupta195 <128774219+AgrimGupta195@users.noreply.github.com>
2025-02-28 06:36:26 +0530

    Fix Url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-    baseURL:import.meta.env.MODE==="development"?"http://localhost:8000/api":"https://accredian-backend-task-k4dd6tazi-agrimgupta195s-projects.vercel.app/api",
+    baseURL:"https://accredian-backend-task-sle1.onrender.com/api",
     withCredentials:true
 })
